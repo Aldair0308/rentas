f14515edaee4a87148793c7726dbb29369ee4fb9
Aldair0308 <133732798+NATHANHERNANDEZ14@users.noreply.github.com>
2024-08-13 18:16:22 -0600

    pago module

--- src/pagos/pagos.service.ts
+++ src/pagos/pagos.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePagoDto } from './dto/create-pago.dto';
 import { UpdatePagoDto } from './dto/update-pago.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pago } from './entities/pago.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PagosService {
-  create(createPagoDto: CreatePagoDto) {
-    return 'This action adds a new pago';
+
+  constructor(
+    @InjectRepository(Pago)
+    private readonly pagoRepository: Repository<Pago>,
+  ){}
+
+  async create(createPagoDto: CreatePagoDto) {
+    const pago = this.pagoRepository.create(createPagoDto);
+    return await this.pagoRepository.save(pago);
   }
 
-  findAll() {
-    return `This action returns all pagos`;
+  async findAll() {
+    return await this.pagoRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pago`;
+  async findOne(id: number): Promise<Pago> {
+    const pago = await this.pagoRepository.findOneBy({ id });
+    if (!pago) {
+      throw new NotFoundException(`Pago con el ID ${id} no se pudo encontrar`);
+    }
+    return pago;
   }
 
-  update(id: number, updatePagoDto: UpdatePagoDto) {
-    return `This action updates a #${id} pago`;
+  async update(id: number, updatePagoDto: UpdatePagoDto) {
+    const pago = await this.pagoRepository.preload({
+      id,
+      ...updatePagoDto,
+    });
+
+    if (!pago) {
+      throw new NotFoundException(`Pago con ID ${id} no se pudo encontrar`);
+    }
+
+    return await this.pagoRepository.save(pago);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pago`;
+  async remove(id: number) {
+    const pago = await this.findOne(id); // Obtener la entidad
+    if (pago) {
+      await this.pagoRepository.remove(pago);
+      return { message: `Pago con ID ${id} ha sido eliminado` };
+    }
   }
 }
+
